e15acbf9e3f30342fe50029c72d1bf0144109cc0
Vasile Creuco <99743470+Vasile-Creuco@users.noreply.github.com>
2022-04-04 21:19:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var digits_of_number = [];
-var bet_numbers = [];
-var sum_money = 0;
-var index = 0;
-var equals = 0;
+let digits_of_dice = [];
+let bet_numbers = [];
+let sum_money = 0;
+let is_equals = 0;
+let equals = 0;
 
 //save the sum of money for bet
 function betMoney() {
@@ -10,28 +10,19 @@ function betMoney() {
     document.getElementById("message").innerText = sum_money;
 }
 
-//save the dice numbers
 function playDice() {
-    var numbers = document.getElementById("numbers").value;
-    let index = 0;
-    while (numbers) {
-        digits_of_number[index] = number % 10;
-        number /= 10;
-        ++index;
+    let first_value = document.getElementById("number_dice").value;
+    let second_value = document.getElementById("number_dice1").value;
+    digits_of_dice[0] = first_value;
+    digits_of_dice[1] = second_value;
+    bet_numbers[0] = Math.floor(Math.random() * 6 + 1);
+    bet_numbers[1] = Math.floor(Math.random() * 6 + 1);
+    if (bet_numbers[0] == digits_of_dice[0] && bet_numbers[1] == digits_of_dice[1]) {
+        equals += 1;
+    } else if (bet_numbers[0] == digits_of_dice[1] && bet_numbers[1] == digits_of_dice[0]) {
+        is_equals += 1;
     }
-}
-
-//check and display whether it is a winner or loser
-function resultGame() {
-    bet_numbers[index] = Math.floor(Math.random() * 6 + 1);
-    ++index;
-    bet_numbers[index] = Math.floor(Math.random() * 6 + 1);
-    for (var i = 0; i < bet_numbers.length; ++i) {
-        if (bet_numbers[i] == digits_of_number[i]) {
-            ++equals;
-        }
-    }
-    if (equals == 2) {
+    if (equals == 1 || is_equals == 1) {
         document.getElementById("message").innerText = "I won " + sum_money * 2 + "!";
     } else {
         document.getElementById("message").innerText = "I lost all money!";
